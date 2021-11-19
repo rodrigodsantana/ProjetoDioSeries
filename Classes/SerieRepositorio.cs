a8ec12c96960339da0fd9d7566fb66d5558e578b
Rodrigo Duarte <rodrygo.santana@hotmail.com>
2021-11-18 22:18:37 -0300

    ProjetoSeriesDio

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio:IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }        
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }          
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }  
+    }
+}
